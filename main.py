cf65c116342ef9d3f090130e2dd370d97a57144b
Aleksiiej <aleksander.ramut@wp.pl>
2023-06-28 22:58:51 +0200

    pacman class added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import pygame
+from pacman import Pacman
 
 
 def main():
@@ -8,11 +9,16 @@ def main():
     win = pygame.display.set_mode((500, 500))
     pygame.display.set_caption("Pacman")
     run = True
+    pacman = Pacman()
 
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        pygame.draw.rect(
+            win, pacman.Colour, (pacman.x, pacman.y, pacman.width, pacman.height)
+        )
+        pygame.display.update()
 
 
 if __name__ == "__main__":
---
+++ pacman.py
@@ -0,0 +1,7 @@
+class Pacman:
+    Colour = [255, 0, 0]
+    x = 10
+    y = 30
+    width = 100
+    height = 100
+    
