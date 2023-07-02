d0ae4554a6971d04f2dccde92021a67b939b551f
Aleksiiej <aleksander.ramut@wp.pl>
2023-07-02 17:28:29 +0200

    Impassable borders added

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python3
 
 import pygame
+from globalValues import *
 from pacman import Pacman
 
 
 def main():
     pygame.init()
-    win = pygame.display.set_mode((500, 500))
+    win = pygame.display.set_mode(SCREENSIZE)
     pygame.display.set_caption("Pacman")
     run = True
     pacman = Pacman()
 
     while run:
-        for event in pygame.event.get():
+        for event in pygame.event.get():  
             if event.type == pygame.QUIT:
                 run = False
+        
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            pacman.moveLeft()
+        if keys[pygame.K_RIGHT]:
+            pacman.moveRight()
+        if keys[pygame.K_UP]:
+            pacman.moveUp()
+        if keys[pygame.K_DOWN]:
+            pacman.moveDown()
+
+        win.fill((0, 0, 0))
         pygame.draw.rect(
             win, pacman.Colour, (pacman.x, pacman.y, pacman.width, pacman.height)
         )
--- pacman.py
+++ pacman.py
@@ -1,7 +1,46 @@
+from globalValues import *
+
 class Pacman:
     Colour = [255, 0, 0]
-    x = 10
+    x = 30
     y = 30
-    width = 100
-    height = 100
+    width = 40
+    height = 40
+
+    def isMoveLeftPossible(self):
+        if self.x > 0:
+            return True
+        else: return False
+
+    def isMoveRightPossible(self):
+        if self.x < SCREENSIZE[0] - self.width:
+            return True
+        else: return False
+    
+    def isMoveUpPossible(self):
+        if self.y > 0:
+            return True
+        else: return False
+    
+    def isMoveDownPossible(self):
+        if self.y < SCREENSIZE[1] - self.height:
+            return True
+        else: return False
+        
+    def moveLeft(self):
+        if self.isMoveLeftPossible():
+            self.x -= 5
+    
+    def moveRight(self):
+        if self.isMoveRightPossible():
+            self.x += 5
+    
+    def moveUp(self):
+        if self.isMoveUpPossible():
+            self.y -= 5
+
+    def moveDown(self):
+        if self.isMoveDownPossible():
+            self.y += 5
+
     
