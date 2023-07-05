e86fcfcb07f25063947d79ed96f25776751778cd
Aleksiiej <aleksander.ramut@wp.pl>
2023-07-05 23:04:17 +0200

    .

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import pygame, sys, time
 from globalValues import *
 from pacman import Pacman
+from map import Map
 
 
 def main():
@@ -11,18 +12,19 @@ def main():
     pygame.display.set_caption("Pacman")
     run = True
     pacman = Pacman()
+    map = Map()
     lastTime = time.time()
-    
-    while run:
 
+    while run:
         dt = time.time() - lastTime
         dt *= 60
         pacman.move(dt)
         lastTime = time.time()
-        
-        for event in pygame.event.get():  
+
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
             pacman.changeDirToLeft()
@@ -35,9 +37,20 @@ def main():
         if keys[pygame.K_ESCAPE]:
             pygame.quit()
             sys.exit()
+
         win.fill((0, 0, 0))
         pygame.draw.rect(
             win, pacman.Colour, (pacman.x, pacman.y, pacman.width, pacman.height)
+        )
+        pygame.draw.rect(
+            win,
+            map.Colour,
+            (
+                map.obstacle1.x,
+                map.obstacle1.y,
+                map.obstacle1.width,
+                map.obstacle1.height,
+            ),
         )
         pygame.display.update()
 
---
+++ map.py
@@ -0,0 +1,5 @@
+import pygame
+
+class Map:
+    Colour = [0, 255, 0]
+    obstacle1 = pygame.Rect(100,100, 20, 20)
