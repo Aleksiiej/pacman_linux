7dcf26fbe7ea2cc16542b384a4467a6e8c5f1a91
Aleksiiej <aleksander.ramut@wp.pl>
2023-07-17 21:01:02 +0200

    .

--- globalValues.py
+++ globalValues.py
@@ -6,13 +6,14 @@ FPS = 60
 
 VELOCITY = 4
 
-BLACK = (0, 0, 0) 
+BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
+
 
 class Direction(Enum):
     UP = 1
     DOWN = 2
     LEFT = 3
-    RIGHT = 4    
+    RIGHT = 4
