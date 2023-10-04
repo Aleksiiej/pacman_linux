61cb01e41c09a607bf5a261f84a152a5c5fb619e
Aleksiiej <aleksander.ramut@wp.pl>
2023-10-04 19:23:52 +0200

    class Apple has now texture as static value

--- apple.py
+++ apple.py
@@ -3,17 +3,15 @@ from globalValues import *
 
 
 class Apple(pygame.sprite.Sprite):
+    image = pygame.transform.scale(
+        pygame.image.load("assets/dot_images/apple.png"), (20, 20)
+    )
+
     def __init__(self, width, height, posX, posY):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface([width, height])
-        self.image = pygame.transform.scale(
-            pygame.image.load("assets/dot_images/apple.png"), (20, 20)
-        )
         self.rect = self.image.get_rect()
         self.rect.center = [posX, posY]
 
     def draw(self, screen):
-            screen.blit(
-                self.image,
-                (self.rect.centerx - 10, self.rect.centery - 10),
-            )
+        screen.blit(Apple.image, (self.rect.x, self.rect.y))
