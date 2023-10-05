517a7ca50abc14e0972e8be654f18da691a29f04
Aleksiiej <aleksander.ramut@wp.pl>
2023-10-05 23:33:05 +0200

    Game Loop pattern implemented

---
+++ game.py
@@ -0,0 +1,61 @@
+import pygame
+from globalValues import *
+from pacman import Pacman
+from scoreCounter import ScoreCounter
+from map import prepareMap
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(SCREENSIZE)
+        pygame.display.set_caption("Pacman")
+        self.clock = pygame.time.Clock()
+
+        self.pacman = Pacman(50, 50, 75, 75, False)
+        self.wallGroup = pygame.sprite.Group()
+        self.appleGroup = pygame.sprite.Group()
+        prepareMap(self.wallGroup, self.appleGroup)
+
+        self.running = True
+
+    def processInput(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.KEYDOWN:
+                match event.key:
+                    case pygame.K_UP:
+                        self.pacman.proposedDir = Direction.UP
+                    case pygame.K_DOWN:
+                        self.pacman.proposedDir = Direction.DOWN
+                    case pygame.K_LEFT:
+                        self.pacman.proposedDir = Direction.LEFT
+                    case pygame.K_RIGHT:
+                        self.pacman.proposedDir = Direction.RIGHT
+                    case pygame.K_ESCAPE:
+                        self.running = False
+
+    def update(self):
+        self.pacman.update(self.wallGroup)
+        for apple in self.appleGroup:
+            if apple.rect.colliderect(self.pacman):
+                self.appleGroup.remove(apple)
+                if len(self.appleGroup) == 0:
+                    self.running = False
+                ScoreCounter.incrementScore()
+                break
+
+    def render(self):
+        self.screen.fill(BLACK)
+        self.pacman.draw(self.screen)
+        self.wallGroup.draw(self.screen)
+        self.appleGroup.draw(self.screen)
+        pygame.display.flip()
+
+    def run(self):
+        while self.running:
+            self.processInput()
+            self.update()
+            self.render()
+            self.clock.tick(FPS)
--- main.py
+++ main.py
@@ -1,51 +1,6 @@
 import pygame
-from globalValues import *
-from pacman import Pacman
-from map import prepareMap
-from drawFacade import DrawFacade
-from scoreCounter import ScoreCounter
+from game import Game
 
-pygame.init()
-screen = pygame.display.set_mode(SCREENSIZE)
-pygame.display.set_caption("Pacman")
-clock = pygame.time.Clock()
-
-pacman = Pacman(50, 50, 75, 75, False)
-wallGroup = pygame.sprite.Group()
-appleGroup = pygame.sprite.Group()
-prepareMap(wallGroup, appleGroup)
-drawFacade = DrawFacade(screen, pacman, wallGroup, appleGroup, pygame)
-
-run = True
-while run:
-    clock.tick(FPS)
-    screen.fill(BLACK)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        if event.type == pygame.KEYDOWN:
-            match event.key:
-                case pygame.K_UP:
-                    pacman.proposedDir = Direction.UP
-                case pygame.K_DOWN:
-                    pacman.proposedDir = Direction.DOWN
-                case pygame.K_LEFT:
-                    pacman.proposedDir = Direction.LEFT
-                case pygame.K_RIGHT:
-                    pacman.proposedDir = Direction.RIGHT
-                case pygame.K_ESCAPE:
-                    run = False
-
-    pacman.update(wallGroup)
-    for apple in appleGroup:
-        if apple.rect.colliderect(pacman):  # only one collision per FPS possible
-            appleGroup.remove(apple)
-            if len(appleGroup) == 0:
-                run = False
-            ScoreCounter.incrementScore()
-            break
-
-    drawFacade.drawGame()
-
+game = Game()
+game.run()
 pygame.quit()
